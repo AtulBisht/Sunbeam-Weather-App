55fb1313f189b807e1ddb6517da8628f5cc588a5
nerd-mac024 <nerd-mac024@nerd-mac024s-MacBook-Pro.local>
2018-05-10 15:14:15 +0530

    solve location issue

--- src/app/wind-graph/wind-graph.component.ts
+++ src/app/wind-graph/wind-graph.component.ts
@@ -25,7 +25,7 @@ export class WindGraphComponent implements OnInit {
     if (sessionStorage.getItem('city') != null) {
       this.cityForecast();
     }
-    else {
+    else if((sessionStorage.getItem('longitude')&& sessionStorage.getItem('latitude')!=null)){
       this.localForecast();
     }
   }
