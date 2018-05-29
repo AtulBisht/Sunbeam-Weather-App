37c69ff68e0122a6f278ab04ce83cd87555ceae6
nerd-mac024 <nerd-mac024@nerd-mac024s-MacBook-Pro.local>
2018-05-29 11:23:17 +0530

    update files

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgProgress } from 'ngx-progressbar';
 
 @Component({
   selector: 'app-sidebar',
